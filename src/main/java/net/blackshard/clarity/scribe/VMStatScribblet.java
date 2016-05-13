604e9ece5c0fd43a369162cf263e768186e09512
Matthew R. Trower <student@blackshard.net>
2016-05-12 23:58:52 -0500

    Add some JavaDoc

--- src/main/java/net/blackshard/clarity/scribe/VMStatScribblet.java
+++ src/main/java/net/blackshard/clarity/scribe/VMStatScribblet.java
@@ -11,10 +11,10 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
+ * Abstract base Scribblet to log a subset of vmstat(1) readings.
+ *
  * @author Matthew R. Trower
- * abstract class VMStatScribblet
- * 
- * Scribblet to log a subset of vmstat(1) readings.
+ * @since 1.0
  */
 public abstract class VMStatScribblet implements Runnable {
     protected Logger log;
@@ -27,11 +27,22 @@ public abstract class VMStatScribblet implements Runnable {
     protected Gatherer gatherer;
     protected VMStatParser parser;
 
+    /**
+     * Default Constructor
+     *
+     * @since 1.0
+     */
     public VMStatScribblet() {
         gatherer = new VMStatGatherer();
         parser = new VMStatParser();
     }
 
+    /**
+     * Run the scribblet.  It will initialize its gathering method and proceed
+     * to gather stats at a set interval, writing them out to the database.
+     *
+     * @since 1.0
+     */
     public void run() {
         log.info(name + ": starting up!");
 
@@ -57,6 +68,12 @@ public abstract class VMStatScribblet implements Runnable {
         gatherer.close();
     }
 
+    /**
+     * Gather and parse stats from the selected gatherer.
+     *
+     * @throws IOException
+     * @since 1.0
+     */
     protected void gatherStats() throws IOException {
         parser.parse(gatherer.read());
         Map<VMStatField, Integer> stats = parser.getStats(fields);
@@ -66,11 +83,23 @@ public abstract class VMStatScribblet implements Runnable {
         reading.setMetrics(toMetricsMap(stats));
     }
 
+    /**
+     * Write the last set of stats collected to the database.
+     *
+     * @throws IOException
+     * @since 1.0
+     */
     protected void writeStats() throws IOException {
         log.debug(String.format("%s: %s", name, reading.getMetrics()));
         dao.insert(reading);
     }
 
+    /**
+     * Convert an array of stats (obtained from VMStatParser) into a metrics map that a Reading DTO will understand.
+     *
+     * @param stats the output from VMStatParser
+     * @return a metrics map that can be fed to Reading.setMetrics()
+     */
     protected abstract LinkedHashMap<String, Integer>
             toMetricsMap(Map<VMStatField, Integer> stats);
 }
